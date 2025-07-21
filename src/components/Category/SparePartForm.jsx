a50f239365fba5fdc948292eff98fa40d28ded61
Shivam <sg641818@gmail.com>
2025-07-21 18:12:52 +0530

    remove unessarey input box from form

--- src/components/Category/SparePartForm.jsx
+++ src/components/Category/SparePartForm.jsx
@@ -168,9 +168,9 @@ const SparePartForm = () => {
       <h2 className="text-xl font-bold text-center">Spare Part</h2>
 
       {renderDropdownWithAddOption("subcategory", "subcategory", pastData.subcategories)}
-      {renderDropdownWithAddOption("brand", "brand", pastData.brands)}
-      {renderDropdownWithAddOption("productName", "product name", pastData.productNames)}
-      {renderDropdownWithAddOption("modelName", "model name", pastData.modelNames)}
+      {renderDropdownWithAddOption("brand", "add machinery name", pastData.brands)}
+      {renderDropdownWithAddOption("productName", "product name ", pastData.productNames)}
+      {/* {renderDropdownWithAddOption("modelName", "model name", pastData.modelNames)} */}
       {renderDropdownWithAddOption("size", "size", pastData.sizes)}
 
       <input type="file" onChange={handleImageChange} className="border p-2" required />
