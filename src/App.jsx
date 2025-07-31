c149b8632c261f6d0014296e9dd1748f1abb9db0
Shivam <sg641818@gmail.com>
2025-07-31 13:56:38 +0530

    resolve bug in updating product

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import OrdersPage from "./Pages/OderPage";
 import QueryList from "./Pages/GetAllQuery";
 import ProfileRequests from "./Pages/ProfileRequests";
 import CategorySelection from "./Pages/CategorySelection";
-import UploadProduct from "./Pages/uploadProduct";
+import Product from "./Pages/Product";
 const PrivateRoute = ({ element }) => {
   const { isAdminLoggedIn } = useAuth();
   return isAdminLoggedIn ? element : <Navigate to="/login" />;
@@ -33,7 +33,7 @@ const App = () => {
           <Route path="/getAllQuery" element={<PrivateRoute element={<QueryList/>} />}/>
           <Route path = "/profile" element={<PrivateRoute element={<ProfileRequests/>}/>}/>
           <Route path ="/category" element={<PrivateRoute element={<CategorySelection/>}/>}/>
-          <Route path ="/uploadProduct" element={<PrivateRoute element={<UploadProduct/>}/>}/>
+          <Route path ="/uploadProduct" element={<PrivateRoute element={<Product/>}/>}/>
           <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </Router>
